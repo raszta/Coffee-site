813035dac27ae608d0978f5b244f811163720f8d
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-06 19:28:27 +0200

    Adding section 2

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ window.addEventListener('load', function(){
 document.querySelector('.navBtn').addEventListener('click', function(){
     document.querySelector('.nav').classList.toggle('nav--show');
 });
+
+//play/stop video
+document.querySelector('.video__switch').addEventListener('click', function(){
+    let btn = document.querySelector('.video__switch-btn');
+    if(!btn.classList.contains('btnSlide')){
+        btn.classList.add('btnSlide');
+        document.querySelector('.video__item').pause();
+    }else{
+        btn.classList.remove('btnSlide');
+        document.querySelector('.video__item').play();
+    }
+});
